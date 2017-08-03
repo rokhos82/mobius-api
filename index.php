8978328fbffee93276eb6d0d5fb82d9c3ff88834
Justin Lane <justin.l.lane@gmail.com>
2017-08-03 16:37:17 -0600

    Installed Silex and setup environment

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+require_once __DIR__.'/../php/vendor/autoload.php';
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+
+$app = new Silex\Application();
+$app['debug'] = true;
+
+$app->get('/',function() {
+  return new Response("This page intentionally left blank.");
+});
+?>
